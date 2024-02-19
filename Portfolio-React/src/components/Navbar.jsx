b8204969ba7469387c6f29e2378d3e878d98be4b
Timbir <tmiddlebrooks99@gmail.com>
2024-02-19 15:58:55 -0600

    added icons to navbar

--- Portfolio-React/src/components/Navbar.jsx
+++ Portfolio-React/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { AiOutlineMenu, AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail } from 'react-icons/ai';
+import { BsPerson } from'react-icons/bs';
+
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -14,12 +16,21 @@ const Navbar = () => {
       <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99]' />
       {nav ? (
         <div className='fixed w-full h-screen bg-white flex flex-col items-center justify-start z-20'>
-          <a href="#main" className='w-[75%] flex justify-center'>
+          <a href="#main" className='w-[50%] flex justify-center items-center rounded-full shadow-lg rounded-full m-2 p-3 cursor-pointer hover:scale-105 ease-in duration-200'>
             <AiOutlineHome size={20} />
             <span className='pl-4'>About Me</span>
-            <span className='pl-4'>Resume</span>
+          </a>
+          <a href="#main" className='w-[50%] flex justify-center items-center rounded-full shadow-lg rounded-full m-2 p-3 cursor-pointer hover:scale-105 ease-in duration-200'>
+           <AiOutlineProject size={20} />
             <span className='pl-4'>Portfolio</span>
+          </a>
+          <a href="#main" className='w-[50%] flex justify-center items-center rounded-full shadow-lg rounded-full m-2 p-3 cursor-pointer hover:scale-105 ease-in duration-200'>
+            <AiOutlineMail size={20} /> 
             <span className='pl-4'>Contact Me</span>
+          </a>
+          <a href="#main" className='w-[50%] flex justify-center items-center rounded-full shadow-lg rounded-full m-2 p-3 cursor-pointer hover:scale-105 ease-in duration-200'>
+            <BsPerson size={20} />
+            <span className='pl-4'>Resume</span>
           </a>
         </div>
       ) : (
