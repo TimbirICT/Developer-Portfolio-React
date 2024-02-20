f012bbf7a2f491e96f91267d599efd92a4929c29
Timbir <tmiddlebrooks99@gmail.com>
2024-02-20 15:42:17 -0600

    homepage/about me near completed

--- Portfolio-React/src/components/Main.jsx
+++ Portfolio-React/src/components/Main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import {TypeAnimation} from 'react-type-animation'
+import CoderImg from './coder.jpg'
+import {FaGithub} from'react-icons/fa'
 
 
 const Main = () => {
   return (
     <div id='main'>
-      <img className='w-full h-screen object-cover' src='./coder.jpg' alt="aesthetic coder" />
+      <img className='w-full h-screen object-cover' src={CoderImg} alt="aesthetic coder" />
       <div className='w-full h-screen absolute top-0 left-0 bg-white/50'> 
-      <div>
-        <h1>I'm Timbir Middlebrooks</h1>
-        <h2>I'm a
+      <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+        <h1 className='sm:text-5xl text-3xl font-bold text-gray-800'>I'm Timbir Middlebrooks</h1>
+        <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a
         <TypeAnimation
       sequence={[
         // Same substring at the start will only be typed out once, initially
@@ -27,6 +29,9 @@ const Main = () => {
       repeat={Infinity}
     />
         </h2>
+        <div className='flex justify-between pt-5 justify-center'>
+          <FaGithub className='cursor-pointer'size={30}/>
+        </div>
       </div>
       </div>
     </div>
